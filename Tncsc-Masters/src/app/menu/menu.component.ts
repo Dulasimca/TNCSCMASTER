f58251a4e18d651e2e7ef423cd8cfe8336df2295
chandrika <udhayachandrikam31@gmail.com>
2020-01-21 17:11:24 +0530

    menu added

--- Tncsc-Masters/src/app/menu/menu.component.ts
+++ Tncsc-Masters/src/app/menu/menu.component.ts
@@ -7,16 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
   showSideBar: boolean;
-  showDownArrowScheme: boolean = false;
+  showDownArrow_SM: boolean = false;
+  showDownArrow_FP: boolean = false;
+  showDownArrow_OM: boolean = false;
+  showDownArrow_CM: boolean = false;
   constructor() { }
 
   ngOnInit() {
-     
-    
+
+
   }
 
-  toggleButton() {
-    this.showDownArrowScheme = !this.showDownArrowScheme;
+  toggleButton(type) {
+    switch (type) {
+      case 'fp':
+        this.showDownArrow_FP = !this.showDownArrow_FP;
+        break;
+      case 'sc':
+        this.showDownArrow_SM = !this.showDownArrow_SM;
+        break;
+      case 'cm':
+        this.showDownArrow_CM = !this.showDownArrow_CM;
+        break;
+      case 'ot':
+        this.showDownArrow_OM = !this.showDownArrow_OM;
+        break;
+    }
   }
 
   onClickMenuBar(event) {
